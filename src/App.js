6a02bdf9c3c8c0830a179fd3c301ac70f599e938
Govindakumar181 <Govindakumar181@gmail.com>
2021-02-15 13:24:37 +0500

    Changes added to check yml workflow action

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello, Good Evening From Govinda!!!</h2>
+        <h3>For Auto Deployment on Surge throw Github Workflow!!!</h3>
         <Dinner Dish="Biryani" SweetDish="Gajjar ja Halwa" />
         <Dinner Dish="Karahi" SweetDish="Ice Cream" />
       </div>
